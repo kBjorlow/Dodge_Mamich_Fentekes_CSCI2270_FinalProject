ff4ffe449722552e555c059bc72d5655aa3771b9
Rmamich <rama7226@colorado.edu>
2016-04-19 16:21:24 -0600

    Create MovieTree.h

---
+++ MovieTree.h
@@ -0,0 +1,49 @@
+#ifndef MOVIETREE_H
+#define MOVIETREE_H
+#include <iostream>
+struct MovieNode{
+    int ranking;
+    std::string title;
+    int year;
+    int quantity;
+    MovieNode *parent;
+    MovieNode *leftChild;
+    MovieNode *rightChild;
+
+    MovieNode(){};
+
+    MovieNode(int in_ranking, std::string in_title, int in_year, int in_quantity)
+    {
+        ranking = in_ranking;
+        title = in_title;
+        year = in_year;
+        quantity = in_quantity;
+    }
+
+};
+
+class MovieTree
+{
+    public:
+        MovieTree();
+        ~MovieTree();
+        void printMovieInventory();
+        int countMovieNodes();
+        void deleteMovieNode(std::string title);
+        void addMovieNode(int ranking, std::string title, int releaseYear, int quantity);
+        void findMovie(std::string title);
+        void rentMovie(std::string title);
+
+    protected:
+    private:
+        void DeleteAll(MovieNode * node); //use this for the post-order traversal deletion of the tree
+        void countMovieNodes(MovieNode *node, int *c);
+        void printMovieInventory(MovieNode * node);
+        MovieNode* search(std::string title);
+        MovieNode* treeMinimum(MovieNode *node);
+        MovieNode *root = NULL;
+
+};
+
+#endif // MOVIETREE_H
+
